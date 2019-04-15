68acc8c46a7ebc19873227346516a2d3da2aad94
NewKid <newkidlp2@gmail.com>
2019-04-15 23:47:08 +0300

    Add login form

--- MyServlet/src/main/webapp/UserServlet.java
+++ MyServlet/src/main/webapp/UserServlet.java
@@ -4,7 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class User extends HttpServlet {
+public class UserServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException, ServletException {
         httpServletRequest.getRequestDispatcher("/User.jsp").forward(httpServletRequest, httpServletResponse);
