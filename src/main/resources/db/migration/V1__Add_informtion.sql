3c7925ede782a4007db6a39badcdc0e2984540a2
FedorSerg <serg300997@gmail.com>
2019-04-21 12:28:24 +0300

    Flyway task done, hope I've chosen the correct branch

---
+++ src/main/resources/db/migration/V1__Add_informtion.sql
@@ -0,0 +1,26 @@
+insert into DRIVER
+values (1, 'Gwion Ritter', '1234567890', '142359708', '89112345678', 'login', 'password'),
+       (2, 'Kyla Williams', '142359708', '4109523876', '13342567676', 'KylalaWill', 'Pa$$w0rd'),
+       (3, 'Reem Wiggins', '4109523876', '9018237465', '89560816741', 'wigginsrt@gmail.com', 'AnthropomorphousHuman19');
+
+insert into TICKET
+values (1, TRUE, 15),
+       (1, FALSE, 20),
+       (2, TRUE, 20);
+
+insert into OFFER
+values (1, 1, 1, 1, 1459, '2019-04-01', '2020-04-01', 'Disapeared'),
+       (2, 2, 2, 2, 20, '2019-03-01', '2019-03-05', 'Finished'),
+       (3, 3, 1, 2, 20, '2019-04-16', '2019-04-20', 'In progress'),
+       (4, 2, 2, 3, 123, '2019-03-01', '2019-04-01', 'Finished'),
+       (5, 3, 1, 4, 8, '2019-04-01', '2019-04-02', 'Finished');
+
+insert into ADMINISTRATOR
+values (1, 'Atticus Burgess', 'Atticus_Burgess', 'averyDifficultpassword'),
+       (2, 'Liyah Walters', 'Liyah_Walters2', 'ThePasswordNumber7');
+
+insert into CAR
+values (1, 'Lada', 'sedan', 'AE25797'),
+       (2, 'Ford', 'hatchback', 'OT84597'),
+       (3, 'Honda', 'offroader', 'BA60697'),
+       (4, 'BMW', 'crossover', 'MK90897');
