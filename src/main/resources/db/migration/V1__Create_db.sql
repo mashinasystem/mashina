2378f8b10ecb9bd50dc043e21be35ae6a5a4cb2d
FedorSerg <serg300997@gmail.com>
2019-04-22 15:28:16 +0300

    Second try

--- src/main/resources/db/migration/V1__Create_db.sql
+++ src/main/resources/db/migration/V1__Create_db.sql
@@ -1,6 +1,6 @@
 create table DRIVER
 (
-    ID        int         not null,
+    ID        int         not null primary key,
     NAME      varchar(64) not null,
     PASSPORT  varchar(10) not null,
     LICENSE   varchar(10) not null,
@@ -11,26 +11,14 @@ create table DRIVER
 
 create table TICKET
 (
-    ID_CLIENT int        not null,
+    ID_CLIENT int     not null references DRIVER (ID),
     IS_PAID   boolean not null,
-    COST      int        not null
-);
-
-create table OFFER
-(
-    ID         int         not null,
-    CLIENT_ID  int         not null,
-    ADMIN_ID   int         not null,
-    CAR_ID     int         not null,
-    PAYMENT    int         not null,
-    BEGIN_DATE varchar(10) not null,
-    END_DATE   varchar(10) not null,
-    STATUS     varchar(512)
+    COST      int     not null
 );
 
 create table ADMINISTRATOR
 (
-    ID       int         not null,
+    ID       int         not null primary key,
     NAME     varchar(64) not null,
     LOGIN    varchar(64) not null,
     PASSWORD varchar(64) not null
@@ -38,8 +26,20 @@ create table ADMINISTRATOR
 
 create table CAR
 (
-    ID     int         not null,
+    ID     int         not null primary key,
     MARQUE varchar(16),
     MODEL  varchar(16),
     NUMBER varchar(10) not null
+);
+
+create table OFFER
+(
+    ID         int         not null primary key,
+    CLIENT_ID  int         not null references DRIVER (ID),
+    ADMIN_ID   int         not null references ADMINISTRATOR (ID),
+    CAR_ID     int         not null references CAR (ID),
+    PAYMENT    int         not null,
+    BEGIN_DATE varchar(10) not null,
+    END_DATE   varchar(10) not null,
+    STATUS     varchar(512)
 );
--- src/main/resources/db/migration/V2__Add_informtion.sql
+++ src/main/resources/db/migration/V2__Add_informtion.sql
@@ -8,19 +8,19 @@ values (1, TRUE, 15),
        (1, FALSE, 20),
        (2, TRUE, 20);
 
+insert into ADMINISTRATOR
+values (1, 'Atticus Burgess', 'Atticus_Burgess', 'averyDifficultpassword'),
+       (2, 'Liyah Walters', 'Liyah_Walters2', 'ThePasswordNumber7');
+
+insert into CAR
+values (1, 'Lada', 'sedan', 'A257EH97'),
+       (2, 'Ford', 'hatchback', 'O845TB97'),
+       (3, 'Honda', 'offroader', 'B606AO97'),
+       (4, 'BMW', 'crossover', 'M908KC97');
+
 insert into OFFER
 values (1, 1, 1, 1, 1459, '2019-04-01', '2020-04-01', 'Disapeared'),
        (2, 2, 2, 2, 20, '2019-03-01', '2019-03-05', 'Finished'),
        (3, 3, 1, 2, 20, '2019-04-16', '2019-04-20', 'In progress'),
        (4, 2, 2, 3, 123, '2019-03-01', '2019-04-01', 'Finished'),
        (5, 3, 1, 4, 8, '2019-04-01', '2019-04-02', 'Finished');
-
-insert into ADMINISTRATOR
-values (1, 'Atticus Burgess', 'Atticus_Burgess', 'averyDifficultpassword'),
-       (2, 'Liyah Walters', 'Liyah_Walters2', 'ThePasswordNumber7');
-
-insert into CAR
-values (1, 'Lada', 'sedan', 'AE25797'),
-       (2, 'Ford', 'hatchback', 'OT84597'),
-       (3, 'Honda', 'offroader', 'BA60697'),
-       (4, 'BMW', 'crossover', 'MK90897');
